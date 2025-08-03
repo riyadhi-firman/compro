cc1ecd56929f7f8454dc77960d361ea8e44d0664
Firman Riyadhi <67591578+riyadhi-firman@users.noreply.github.com>
2025-08-03 21:10:26 +0700

    Resize Logo to Size 25

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -30,7 +30,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 <img 
                   src="/logo-mmd.png" 
                   alt="MMD Logo" 
-                  className="w-35 h-35 object-contain"
+                  className="w-25 h-25 object-contain"
                 />
               </Link>
             </div>
